9fcc063d21b971bb9891b2ab652f7b9c0a8ff33c
Seth Bashford <seth.bashford@boystown.org>
2019-07-24 11:23:33 -0500

    adding some finishing example impl

--- chapro-openmha-plugin/chapro-openmha-plugin/chapro-openmha-plugin.cpp
+++ chapro-openmha-plugin/chapro-openmha-plugin/chapro-openmha-plugin.cpp
@@ -116,94 +116,45 @@ int Chapro::channels() {
     return channels_;
 }
 
-/** This C++ class implements the simplest example plugin for the
- * step-by-step tutorial.  It inherits from MHAPlugin::plugin_t for
- * correct integration in the configuration language interface.  */
-class example1_t : public MHAPlugin::plugin_t<int> {
+std::shared_ptr<hearing_aid::FilterbankCompressor> compressor() {
+    hearing_aid::FilterbankCompressor::Parameters p;
+    p.channels = 8;
+    p.attack_ms = 5;
+    p.release_ms = 50;
+    p.chunkSize = 64;
+    p.max_dB_Spl = 119;
+    p.sampleRate = 44100;
+    p.windowSize = 256;
+    p.compressionRatios = { 1.1, 1.1, 1.2, 1.1, 1.5, 1.6, 1.6, 2.1 };
+    p.broadbandOutputLimitingThresholds_dBSpl =
+        { 100, 100, 100, 100, 100, 100, 100, 100 };
+    p.crossFrequenciesHz =
+        { 317, 503, 798, 1265, 2006, 3181, 5045 };
+    p.kneepointGains_dB =
+        { 2, 2, 10, 2, 27, 36, 34, 49 };
+    p.kneepoints_dBSpl =
+        { 32, 32, 37, 29, 38, 39, 35, 39 };
+    return std::make_shared<Chapro>(std::move(p));
+}
+
+class ChaproOpenMhaPlugin : public MHAPlugin::plugin_t<int> {
+    hearing_aid::HearingAid hearingAid;
 public:
-  /** Do-nothing constructor.  The constructor has to take these three
-   * arguments, but it does not have to use them. However, the base
-   * class has to be initialized. */
-  example1_t(algo_comm_t & ac,
-            const std::string &,
-            const std::string &)
-      : MHAPlugin::plugin_t<int>("",ac)
-  {/* Do nothing in constructor */}
-
-  /** Release may be empty */
-  void release(void)
-  {/* Do nothing in release */}
-
-  /** Plugin preparation. This plugin checks that the input signal has the
-   * waveform domain and contains at least one channel
-   * @param signal_info
-   *   Structure containing a description of the form of the signal (domain,
-   *   number of channels, frames per block, sampling rate.
-   */
-  void prepare(mhaconfig_t & signal_info)
-  {
-    if (signal_info.domain != MHA_WAVEFORM)
-      throw MHA_Error(__FILE__, __LINE__,
-                      "This plugin can only process waveform signals.");
-    if (signal_info.channels < 1)
-      throw MHA_Error(__FILE__,__LINE__,
-                      "This plugin requires at least one input channel.");
-  }
-
-  /** Signal processing performed by the plugin.
-   * This plugin multiplies the signal in the first audio channel by
-   * a factor 0.1.
-   * @param signal
-   *   Pointer to the input signal structure.
-   * @return
-   *   Returns a pointer to the input signal structure,
-   *   with a the signal modified by this plugin.
-   *   (In-place processing)
-   */
-  mha_wave_t * process(mha_wave_t * signal)
-  {
-    unsigned int channel = 0; // channels and frames counting starts with 0
-    float factor = 0.1f;
-    unsigned int frame;
-
-    // Scale channel number "channel" by "factor":
-    for(frame = 0; frame < signal->num_frames; frame++) {
-      // Waveform channels are stored interleaved.
-      signal->buf[signal->num_channels * frame + channel] *= factor;
+    ChaproOpenMhaPlugin(
+        algo_comm_t &ac,
+        const std::string &,
+        const std::string &
+    ) :
+        MHAPlugin::plugin_t<int>{{}, ac},
+        hearingAid{compressor()} {}
+    
+    mha_wave_t *process(mha_wave_t * signal) {
+        hearingAid.process({signal->buf, signal->num_frames});
+        return signal;
     }
-    // Algorithms may process data in-place and return the input signal
-    // structure as their output signal:
-    return signal;
-  }
+    
+    void prepare(mhaconfig_t &) override {}
 };
 
-/*
- * This macro connects the example1_t class with the \mha plugin C interface
- * The first argument is the class name, the other arguments define the
- * input and output domain of the algorithm.
- */
-MHAPLUGIN_CALLBACKS(example1,example1_t,wave,wave)
-
-/*
- * This macro creates code classification of the plugin and for
- * automatic documentation.
- *
- * The first argument to the macro is a space separated list of
- * categories, starting with the most relevant category. The second
- * argument is a LaTeX-compatible character array with some detailed
- * documentation of the plugin.
- */
-MHAPLUGIN_DOCUMENTATION\
-(example1,
- "example level-modification audio-channels",
- "The {\\bf simplest} example of an \\mha{} plugin.\n\n"
- "This plugin scales one channel of the input signal, working in the "
- "time domain."
- )
-
-// Local Variables:
-// compile-command: "make"
-// c-basic-offset: 4
-// indent-tabs-mode: nil
-// coding: utf-8-unix
-// End:
+MHAPLUGIN_CALLBACKS(chapro, ChaproOpenMhaPlugin, wave, wave)
+MHAPLUGIN_DOCUMENTATION(chapro, "text", "here")
