e9b4715aaae579718776c70829481b60c25b421d
Seth Bashford <seth.bashford@boystown.org>
2019-08-29 15:03:17 -0500

    add test

--- chapro-openmha-plugin/google-tests/AfcHearingAidTests.cpp
+++ chapro-openmha-plugin/google-tests/AfcHearingAidTests.cpp
@@ -1,11 +1,114 @@
+#include <gsl/gsl>
+#include <memory>
+
+class SuperSignalProcessor {
+public:
+    using real_type = float;
+    virtual ~SuperSignalProcessor() = default;
+    virtual void feedbackCancelInput() = 0;
+    virtual void compressInput() = 0;
+    virtual void filterbankAnalyze() = 0;
+    virtual void compressChannel() = 0;
+    virtual void filterbankSynthesize() = 0;
+    virtual void compressOutput() = 0;
+    virtual void feedbackCancelOutput() = 0;
+};
+
+class AfcHearingAid {
+    std::shared_ptr<SuperSignalProcessor> processor;
+public:
+    using signal_type = gsl::span<SuperSignalProcessor::real_type>;
+    explicit AfcHearingAid(
+        std::shared_ptr<SuperSignalProcessor> processor
+    ) : processor{std::move(processor)} {}
+
+    void process(signal_type) {
+        processor->feedbackCancelInput();
+        processor->compressInput();
+        processor->filterbankAnalyze();
+        processor->compressChannel();
+        processor->filterbankSynthesize();
+        processor->compressOutput();
+        processor->feedbackCancelOutput();
+    }
+};
+
 #include <gtest/gtest.h>
 
-namespace {
-class AfcHearingAidTests : public ::testing::Test {
+namespace {    
+void assertEqual(const std::string &expected, const std::string &actual) {
+    EXPECT_EQ(expected, actual);
+}
 
+class LogString {
+    std::stringstream s{};
+public:
+    void insert(std::string s_) {
+        s << std::move(s_);
+    }
+
+    bool isEmpty() const {
+        return s.str().empty();
+    }
+    
+    operator std::string() const { return s.str(); }
+};
+
+class SuperSignalProcessorStub : public SuperSignalProcessor {
+    LogString log_;
+public:
+    auto &log() const {
+        return log_;
+    }
+
+    void feedbackCancelInput() {
+        log_.insert("feedbackCancelInput");
+    }
+    void compressInput() {
+        log_.insert("compressInput");
+    }
+    void filterbankAnalyze() {
+        log_.insert("filterbankAnalyze");
+    }
+    void compressChannel() {
+        log_.insert("compressChannel");
+    }
+    void filterbankSynthesize() {
+        log_.insert("filterbankSynthesize");
+    }
+    void compressOutput() {
+        log_.insert("compressOutput");
+    }
+    void feedbackCancelOutput() {
+        log_.insert("feedbackCancelOutput");
+    }
+};
+
+class AfcHearingAidTests : public ::testing::Test {
+    std::shared_ptr<SuperSignalProcessorStub> superSignalProcessor = 
+        std::make_shared<SuperSignalProcessorStub>();
+    AfcHearingAid hearingAid{superSignalProcessor};
+protected:
+    auto &signalProcessingLog() {
+        return superSignalProcessor->log();
+    }
+
+    void process() {
+        hearingAid.process({});
+    }
 };
 
 TEST_F(AfcHearingAidTests, tbd) {
-    FAIL();
+    process();
+    assertEqual(
+        "feedbackCancelInput"
+        "compressInput"
+        "filterbankAnalyze"
+        "compressChannel"
+        "filterbankSynthesize"
+        "compressOutput"
+        "feedbackCancelOutput",
+        signalProcessingLog()
+    );
 }
 }
