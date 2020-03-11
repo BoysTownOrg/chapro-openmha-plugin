f277df3fac74f29b6b85846005de993322ae5e96
sbashford <sbashford@users.noreply.github.com>
2020-03-11 16:35:51 -0500

    Update chapro 3 11 20 (#30)
    * use updated chapro and gcc-8 for cross compilers
    
    * trying to update ar
    
    * again...

---
+++ Toolchain-arm-linux-gnueabihf-8.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc-8)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-8)
+set(CMAKE_AR arm-linux-gnueabihf-ar CACHE FILEPATH "Archiver")
