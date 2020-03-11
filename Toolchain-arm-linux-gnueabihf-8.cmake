35c5663f99176fe6496d268d168c076bcad24a3c
Seth Bashford <seth.bashford@boystown.org>
2020-03-11 16:30:55 -0500

    again...

--- Toolchain-arm-linux-gnueabihf-8.cmake
+++ Toolchain-arm-linux-gnueabihf-8.cmake
@@ -3,4 +3,4 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc-8)
 set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-8)
-set(CMAKE_AR arm-linux-gnueabihf-ar)
+set(CMAKE_AR arm-linux-gnueabihf-ar CACHE FILEPATH "Archiver")
